86c9f2dff457c5a4549f30449550416ca07fe9cf
JoJoJux <80523427+J0J0HA@users.noreply.github.com>
2023-08-27 13:44:56 +0200

    COmbined all functions in "gterminal" object

--- modules.js
+++ modules.js
@@ -1,10 +1,10 @@
-const main = registerModule(
+const main = gterminal.modules.register(
     "main",
     "Main",
     "Default commands"
 )
 
-const modulesman = registerModule(
+const modulesman = gterminal.modules.register(
     "modules",
     "Module Manager",
     "Manage modules"
@@ -13,24 +13,24 @@ const modulesman = registerModule(
 modulesman.registerCommand("modules", "Manage modules", async (full, rest) => {
     let srest = rest.split(" ");
     if (srest[0] == "tmpinstall") {
-        loadFile(srest[1]);
-        out("File was added for this session.");
+        gterminal.utils.loadFile(srest[1]);
+        gterminal.io.println("File was added for this session.");
     }
     else if (srest[0] == "install") {
-        loadFile(srest[1]);
+        gterminal.utils.loadFile(srest[1]);
         let module_list = JSON.parse(localStorage.getItem("modules") || "[]");
         module_list.push(srest[1]);
         localStorage.setItem("modules", JSON.stringify(module_list));
-        out("Module was added.");
+        gterminal.io.println("Module was added.");
     }
     else if (srest[0] == "list") {
         let module_list = JSON.parse(localStorage.getItem("modules") || "[]");
-        out("These 3rd-party modules are installed globally:");
+        gterminal.io.println("These 3rd-party modules are installed globally:");
         for (let module in module_list) {
-            out(`[${module}] ${module_list[module]}`)
+            gterminal.io.println(`[${module}] ${module_list[module]}`)
         }
-        out("You can use 'y [index]' to uninstall the corresponding module.");
-        y_command = "modules uninstall-index ";
+        gterminal.io.println("You can use 'y [index]' to uninstall the corresponding module.");
+        gterminal.commands.setY("modules uninstall-index ");
     }
     else if (srest[0] == "uninstall") {
         let module_list = JSON.parse(localStorage.getItem("modules") || "[]");
@@ -38,9 +38,9 @@ modulesman.registerCommand("modules", "Manage modules", async (full, rest) => {
         if (index !== -1) {
             module_list.splice(index, 1);
             localStorage.setItem("modules", JSON.stringify(module_list));
-            out("Module was removed.");
+            gterminal.io.println("Module was removed.");
         } else {
-            out("Module not found.");
+            gterminal.io.println("Module not found.");
         }
     }
     else if (srest[0] == "uninstall-index") {
@@ -48,69 +48,69 @@ modulesman.registerCommand("modules", "Manage modules", async (full, rest) => {
         if (module_list.length > parseInt(srest[1])) {
             module_list.splice(parseInt(srest[1]), 1);
             localStorage.setItem("modules", JSON.stringify(module_list));
-            out("Module was removed.");
+            gterminal.io.println("Module was removed.");
         } else {
-            out("Module not found.");
+            gterminal.io.println("Module not found.");
         }
     }
 })
 
 main.registerCommand("this", "Open this project on github", async (full, rest) => {
-    window.modifyHref("https://github.com/gterminal-project/");
-    out("Please wait...");
+    gterminal.web.goto("https://github.com/gterminal-project/");
+    gterminal.io.println("Please wait...");
 })
 
 main.registerCommand("y", "Execute the y command", async (full, rest) => {
-    runCommand(y_command + rest);
+    gterminal.commands.exec(y_command + rest);
 })
 
 main.registerCommand("copy", "Run this to copy if copying failed before", async (full, rest) => {
     if (rest) {
-        copyToClipboard(rest);
+        gterminal.clipboard.copy(rest);
     } else {
-        copyToClipboard(last_copy);
+        gterminal.clipboard.recopy();
     }
-    out(`Copied to clipboard: ${last_copy}`);
+    gterminal.io.println(`Copied to clipboard: ${gterminal.clipboard.last_copy}`);
 })
 
 main.registerCommand("do", "Execute the following command", async (full, rest) => {
-    runCommand(rest);
+    gterminal.commands.exec(rest);
 })
 
 main.registerCommand("help", "Show availible commands or the description of a specific command", async (full, rest) => {
     if (rest) {
-        out(commands[rest].description)
+        gterminal.io.println(commands[rest].description)
     } else {
-        out("The following commands are availible:");
+        gterminal.io.println("The following commands are availible:");
         for (let [command, details] of Object.entries(commands)) {
-            out(`- ${command} (${details.description})`);
+            gterminal.io.println(`- ${command} (${details.description})`);
         }
     }
 })
 
 main.registerCommand("open", "Open the provided href", async (full, rest) => {
-    window.modifyHref(rest);
-    out("Please wait...");
+    gterminal.web.goto(rest);
+    gterminal.io.println("Please wait...");
 })
 
 main.registerCommand("sleep", "Do nothing and relax for the given amount of ms", async (full, rest) => {
-    await sleep(parseInt(rest));
+    await gterminal.utils.sleep(parseInt(rest));
 })
 
-loadFile("https://gterminal.is-a.dev/modules/module/github.js");
-loadFile("https://gterminal.is-a.dev/modules/module/you.js");
-loadFile("https://gterminal.is-a.dev/modules/module/google.js");
-loadFile("https://gterminal.is-a.dev/modules/module/openai.js");
-loadFile("https://gterminal.is-a.dev/modules/module/shortlinks.js");
-loadFile("https://gterminal.is-a.dev/modules/module/youtube.js");
-loadFile("https://gterminal.is-a.dev/modules/module/twitch.js");
-loadFile("https://gterminal.is-a.dev/modules/module/webdev.js");
+gterminal.utils.loadFile("https://gterminal.is-a.dev/modules/module/github.js");
+gterminal.utils.loadFile("https://gterminal.is-a.dev/modules/module/you.js");
+gterminal.utils.loadFile("https://gterminal.is-a.dev/modules/module/google.js");
+gterminal.utils.loadFile("https://gterminal.is-a.dev/modules/module/openai.js");
+gterminal.utils.loadFile("https://gterminal.is-a.dev/modules/module/shortlinks.js");
+gterminal.utils.loadFile("https://gterminal.is-a.dev/modules/module/youtube.js");
+gterminal.utils.loadFile("https://gterminal.is-a.dev/modules/module/twitch.js");
+gterminal.utils.loadFile("https://gterminal.is-a.dev/modules/module/webdev.js");
 
 for (let url of JSON.parse(localStorage.getItem("modules") || "[]")) {
-    loadFile(url);
+    gterminal.utils.loadFile(url);
 }
 
 main.registerCommand("command_not_found", "[INTERNAL] Show notice that a command was not found.", async (full, rest) => {
-    out(`The command "${rest}" could not be found. Did you mean "help"?`);
-    y_command = "help"
+    gterminal.io.println(`The command "${rest}" could not be found. Did you mean "help"?`);
+    gterminal.commands.setY("help");
 })
