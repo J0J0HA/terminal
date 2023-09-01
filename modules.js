0ebea198353373f5693369e00f1079af678caa82
JoJoJux <80523427+J0J0HA@users.noreply.github.com>
2023-09-01 21:21:11 +0200

    Adjusted to match new module API

--- modules.js
+++ modules.js
@@ -6,23 +6,65 @@
 
 /**
  * @type {InstalledModule}
+ * @private
  */
+
+gterminal.modules.repos["_"] = new Repo(gterminal, "_", "_", {
+    "main": {
+        "name": "Main",
+        "description": "Default commands",
+        "script": "#",
+        "commands": {
+            "this": {
+                "description": "?"
+            },
+            "y": {
+                "description": "?"
+            },
+            "copy": {
+                "description": "?"
+            },
+            "open": {
+                "description": "?"
+            },
+            "help": {
+                "description": "?"
+            },
+            "do": {
+                "description": "?"
+            },
+            "sleep": {
+                "description": "?"
+            }
+        }
+    },
+    "modules": {
+        "name": "Module Manager",
+        "description": "Module management commands",
+        "script": "#",
+        "commands": {
+            "modules": {
+                "description": "Module management command"
+            }
+        }
+    }
+})
+
 const main = gterminal.modules.register(
-    "main",
-    "Main",
-    "Default commands"
+    "_:main"
 )
 
 /**
  * @type {InstalledModule}
+ * @private
  */
 const modules = gterminal.modules.register(
-    "modules",
-    "Module Manager",
-    "Manage modules"
+    "_:modules"
 )
 
-modules.registerCommand("modules", "Manage modules", async (full, rest) => {
+console.log(main, modules);
+
+modules.registerCommand("modules", async (full, rest) => {
     let srest = rest.split(" ");
     if (srest[0] == "tmpinstall") {
         gterminal.utils.loadFile(srest[1]);
@@ -50,7 +92,7 @@ modules.registerCommand("modules", "Manage modules", async (full, rest) => {
         gterminal.io.println("Installed modules:");
         for (let module of Object.values(gterminal.modules.modules)) {
             gterminal.io.println(`- ${module.name} [${module.id}]`);
-        } 
+        }
         if (gterminal.modules.modules.length == 0) {
             gterminal.io.println("No modules installed.");
         }
@@ -65,16 +107,16 @@ modules.registerCommand("modules", "Manage modules", async (full, rest) => {
     gterminal.config.save();
 })
 
-main.registerCommand("this", "Open this project on github", async (full, rest) => {
+main.registerCommand("this", async (full, rest) => {
     gterminal.web.goto("https://github.com/gterminal-project/");
     gterminal.io.println("Please wait...");
 })
 
-main.registerCommand("y", "Execute the y command", async (full, rest) => {
+main.registerCommand("y", async (full, rest) => {
     gterminal.commands.exec(y_command + rest);
 })
 
-main.registerCommand("copy", "Run this to copy if copying failed before", async (full, rest) => {
+main.registerCommand("copy", async (full, rest) => {
     if (rest) {
         gterminal.clipboard.copy(rest);
     } else {
@@ -83,61 +125,30 @@ main.registerCommand("copy", "Run this to copy if copying failed before", async 
     gterminal.io.println(`Copied to clipboard: ${gterminal.clipboard.last_copy}`);
 })
 
-main.registerCommand("do", "Execute the following command", async (full, rest) => {
+main.registerCommand("do", async (full, rest) => {
     gterminal.commands.exec(rest);
 })
 
-main.registerCommand("help", "Show availible commands or the description of a specific command", async (full, rest) => {
+main.registerCommand("help", async (full, rest) => {
     if (rest) {
-        gterminal.io.println(commands[rest].description)
+        gterminal.io.println(gterminal.commands.exec)
     } else {
         gterminal.io.println("The following commands are availible:");
-        for (let [command, details] of Object.entries(commands)) {
-            gterminal.io.println(`- ${command} (${details.description})`);
+        let commands = [];
+        for (let module of Object.values(gterminal.modules.modules)) {
+            for (let command of Object.values(module.commands)) {
+                gterminal.io.println(`- ${command.id} (${command.description}) [${command.module.id}]`);
+            }
         }
     }
 })
 
-main.registerCommand("open", "Open the provided href", async (full, rest) => {
+main.registerCommand("open", async (full, rest) => {
     gterminal.web.goto(rest);
     gterminal.io.println("Please wait...");
 })
 
-main.registerCommand("sleep", "Do nothing and relax for the given amount of ms", async (full, rest) => {
+main.registerCommand("sleep", async (full, rest) => {
     await gterminal.utils.sleep(parseInt(rest));
 })
-
-let repo_list = gterminal.config.get("added_repos", [
-    "https://gterminal.is-a.dev/modules/repo.json"
-]);
-gterminal.config.set("added_repos", repo_list);
-
-let module_list = gterminal.config.get("installed_modules", [
-    // "main:github",
-    // "main:you",
-    // "main:google",
-    // "main:openai",
-    // "main:shortlinks",
-    // "main:youtube",
-    // "main:twitch",
-    // "main:webdev"
-]);
-gterminal.config.set("installed_modules", module_list);
-gterminal.config.save();
-
-(async () => {
-    for (let repo in repo_list) {
-        await gterminal.modules.loadRepo(repo_list[repo]);
-    }
-
-    for (let module in module_list) {
-        await gterminal.modules.loadModule(module_list[module]);
-    }
-    
-    main.registerCommand("command_not_found", "[INTERNAL] Show notice that a command was not found.", async (full, rest) => {
-        gterminal.io.println(`The command "${rest}" could not be found. Did you mean "help"?`);
-        gterminal.commands.setY("help");
-    })
-})()
-
 
